fbd7c0da0f8df708af749e52747d087001c660ff
ppkt <github@ppkt.eu>
2015-07-04 19:40:23 +0200

    basic support for rf24l01 module

--- rf24l01.c
+++ rf24l01.c
@@ -1,14 +1,114 @@
 #include "rf24l01.h"
 
-void rf24l01_init(void) {
+u8 tx[32];
+u8 rx[32];
+
+void rf24l01_gpio_init() {
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    // Configure RF24 pins: CE, IRQ
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    rf24l01_ce(0);
+
+}
+
+void rf24l01_ce(bool new_state) {
+    if (new_state) {
+        GPIO_WriteBit(GPIOA, GPIO_Pin_2, Bit_SET);
+    } else {
+        GPIO_WriteBit(GPIOA, GPIO_Pin_2, Bit_RESET);
+    }
+}
+
+void rf24l01_init(bool receiver) {
     // Initialize SPI1
     spi_init();
+
+    // Prepare timer
+    setup_delay_timer(TIM3);
+
+    u8 init_buffer[5] = {0x00};
+
+    init_buffer[0] = 0x01;
+    rf24l01_write_register(EN_AA, init_buffer, 1); // enable auto-ack on pipe 0
+
+    init_buffer[0] = 0x01;
+    rf24l01_write_register(EN_RXADDR, init_buffer, 1); // enable data pipe 0
+
+    init_buffer[0] = 0x03;
+    rf24l01_write_register(SETUP_AW, init_buffer, 1); // 5 bytes RF address
+
+    init_buffer[0] = 0x2F;
+    rf24l01_write_register(SETUP_RETR, init_buffer, 1); // 750us retransmission delay, up to 15 retransmissions
+
+    init_buffer[0] = 0x01;
+    rf24l01_write_register(RF_CH, init_buffer, 1); // 2.401 GHz
+
+    init_buffer[0] = 0x07;
+    rf24l01_write_register(RF_SETUP, init_buffer, 1); // 1Mbps, -0dB
+
+    // set unique address of device
+    u8 *ptr = (uint8_t*)U_ID_PTR;
+    init_buffer[0] = *(ptr++);
+    init_buffer[1] = *(ptr++);
+    init_buffer[2] = *(ptr++);
+    init_buffer[3] = *(ptr++);
+    init_buffer[4] = *(ptr++);
+    printf("Device address: %02X %02X %02X %02X %02X\n\r", init_buffer[0], init_buffer[1],
+            init_buffer[2], init_buffer[3], init_buffer[4]);
+    rf24l01_write_register(RX_ADDR_P0, init_buffer, 5);
+    rf24l01_write_register(TX_ADDR, init_buffer, 5); // must be the same if EN_AA is enabled
+    rf24l01_read_register(RX_ADDR_P0, init_buffer, 5);
+
+    init_buffer[0] = 5;
+    rf24l01_write_register(RX_PW_P0, init_buffer, 1); // set payload size to 5 bytes
+
+    init_buffer[0] = 0x0E;
+    if (receiver) {
+        init_buffer[0] |= 0x01;
+    }
+    rf24l01_write_register(CONFIG, init_buffer, 1); // enable crc, use 2 bytes, power up
+
+
+    GPIO_WriteBit(GPIOA, GPIO_Pin_1, Bit_RESET);
+    GPIO_WriteBit(GPIOA, GPIO_Pin_1, Bit_SET);
+    delay_us(TIM3, 150);
+    GPIO_WriteBit(GPIOA, GPIO_Pin_1, Bit_RESET);
+    GPIO_WriteBit(GPIOA, GPIO_Pin_1, Bit_SET);
 }
 
 u8 rf24l01_status(void) {
-    u8 tx[1] = {0xFF}; // NOP
-    u8 rx[1] = {0x00};
+    tx[0] = NOP;
 
     spi_send(tx, rx, 1);
     return rx[0];
+}
+
+void rf24l01_write_register(u8 addr, u8 *tx_buffer, u8 bytes) {
+    u16 j;
+    for (j = 0; j < 200; ++j) {}
+    tx[0] = W_REGISTER | addr;
+    memcpy(tx+1, tx_buffer, bytes);
+
+
+    spi_send(tx, rx, bytes + 1);
+}
+
+// Reads `bytes` bytes from `addr` to `rx_buffer`. Returns Status register
+u8 rf24l01_read_register(u8 addr, u8 *rx_buffer, u8 bytes) {
+    u16 j;
+    for (j = 0; j < 200; ++j) {}
+    memset(tx, 0, bytes + 1);
+    tx[0] = R_REGISTER | addr;
+    spi_send(tx, rx_buffer, bytes + 1);
+    return rx_buffer[0];
 }
--- rf24l01.h
+++ rf24l01.h
@@ -1,9 +1,51 @@
 #ifndef __RF24L01_H__
 #define __RF24L01_H__
 
-#include "common_lib/spi.h"
+#include "stdbool.h"
+#include "stdio.h"
+#include "string.h"
 
-void rf24l01_init(void);
+#include "common_lib/spi.h"
+#include "common_lib/utils.h"
+
+// Commands
+#define R_REGISTER  0b00000000
+#define W_REGISTER  0b00100000
+#define NOP         0b11111111
+
+// Registers
+#define CONFIG      0x00
+#define EN_AA       0x01
+#define EN_RXADDR   0x02
+#define SETUP_AW    0x03
+#define SETUP_RETR  0x04
+#define RF_CH       0x05
+#define RF_SETUP    0x06
+#define STATUS      0x07
+#define OBSERVE_TX  0x08
+#define RPD         0x09
+
+// Register below - 5 bytes length
+#define TX_ADDR     0x10
+#define RX_ADDR_P0  0x0A
+#define RX_ADDR_P1  0x0B
+
+#define RX_ADDR_P2  0x0C
+#define RX_ADDR_P3  0x0D
+#define RX_ADDR_P4  0x0E
+#define RX_ADDR_P5  0x0F
+#define RX_PW_P0    0x11
+#define RX_PW_P1    0x12
+#define RX_PW_P2    0x13
+#define RX_PW_P3    0x14
+#define RX_PW_P4    0x15
+#define RX_PW_P5    0x16
+#define FIFO_STATUS 0x17
+
+void rf24l01_init(bool receiver);
 u8 rf24l01_status(void);
+void rf24l01_ce(bool new_state);
+void rf24l01_write_register(u8 addr, u8 *tx_buffer, u8 bytes);
+u8 rf24l01_read_register(u8 addr, u8 *rx_buffer, u8 bytes);
 
 #endif // __RF24L01_H__
