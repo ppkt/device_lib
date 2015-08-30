a4c61beb42e7b17eaff7c940eb4315cf56792905
Karol Werner <karol@ppkt.eu>
2015-08-30 17:26:30 +0200

    RDA5807_TEA: Small refactoring

--- rda5807_tea.h
+++ rda5807_tea.h
@@ -1,28 +1,15 @@
 #ifndef __RDA5807_TEA_H__
 #define __RDA5807_TEA_H__
-#include "i2c_dma.h"
-#include "utils.h"
+
 #include <stdio.h>
 #include <stdbool.h>
+
+#include "common_lib/i2c_dma.h"
+#include "common_lib/utils.h"
 
 // Commands for module in TEA5767HN compability mode
 
 #define RDA5970_TEA_ADDRESS 0x60
-
-typedef struct radio {
-    char *name;
-    unsigned int frequency;
-    struct radio *next;
-    struct radio *prev;
-} radio;
-
-typedef struct {
-    radio *head;
-    radio *tail;
-    radio *current;
-} radio_list;
-
-radio_list *stations;
 
 void rda5807_tea_init();
 void rda5807_tea_set_frequency(unsigned int new_frequency);
