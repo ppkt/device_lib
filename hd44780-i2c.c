e78850997452353410b8da9cd1e51cd6295a7404
Karol Werner <karol@ppkt.eu>
2015-09-10 19:30:42 +0200

    HD44780: Replacing tabs by spaces + exposing cgram_write in header

--- hd44780-i2c.c
+++ hd44780-i2c.c
@@ -24,58 +24,58 @@ TIM_TypeDef *timer;
 uint8_t hd44780_data[4] = {0x00, 0x00, 0x00, 0x00};
 
 void hd44780_send(u8 cmd, bool set_rs) {
-	u8 rs_ = 0;
-	if (set_rs)
-		rs_ = rs;
+    u8 rs_ = 0;
+    if (set_rs)
+    	rs_ = rs;
 
-	hd44780_data[0] = (cmd & 0xF0) | backlight | en | rs_;
-	hd44780_data[1] = (cmd & 0xF0) | backlight;
-	hd44780_data[2] = (cmd & 0x0F) << 4 | backlight | en | rs_;
-	hd44780_data[3] = (cmd & 0x0F) << 4 | backlight;
-	I2C_Master_BufferWrite(I2C1, hd44780_data, 4, Polling, hd44780_address << 1);
+    hd44780_data[0] = (cmd & 0xF0) | backlight | en | rs_;
+    hd44780_data[1] = (cmd & 0xF0) | backlight;
+    hd44780_data[2] = (cmd & 0x0F) << 4 | backlight | en | rs_;
+    hd44780_data[3] = (cmd & 0x0F) << 4 | backlight;
+    I2C_Master_BufferWrite(I2C1, hd44780_data, 4, Polling, hd44780_address << 1);
 }
 
 void hd44780_char(u8 cmd) {
-	hd44780_send(cmd, true);
-	delay_us(timer, 200);
+    hd44780_send(cmd, true);
+    delay_us(timer, 200);
 }
 
 void hd44780_cmd(u8 cmd) {
-	hd44780_send(cmd, false);
-	delay_us(timer, 5000);
+    hd44780_send(cmd, false);
+    delay_us(timer, 5000);
 }
 
 void hd44780_print(char *string) {
-	char* ptr = string;
-	while (*(ptr) != 0) {
-		hd44780_char(*ptr++);
-	};
+    char* ptr = string;
+    while (*(ptr) != 0) {
+        hd44780_char(*ptr++);
+    };
 }
 
 void hd44780_backlight(bool new_value) {
-	backlight = new_value << 3;
-	I2C_Master_BufferRead(I2C1, hd44780_data, 1, Polling, hd44780_address << 1);
-	hd44780_data[0] |= backlight;
-	I2C_Master_BufferWrite(I2C1, hd44780_data, 1, Polling, hd44780_address << 1);
+    backlight = new_value << 3;
+    I2C_Master_BufferRead(I2C1, hd44780_data, 1, Polling, hd44780_address << 1);
+    hd44780_data[0] |= backlight;
+    I2C_Master_BufferWrite(I2C1, hd44780_data, 1, Polling, hd44780_address << 1);
 }
 
 void hd44780_go_to_line(u8 line) {
-	switch(line) {
-	case 0:
-		hd44780_cmd(0x80);
-		break;
-	case 1:
-		hd44780_cmd(0xC0);
-		break;
-	case 2:
-		hd44780_cmd(0x94);
-		break;
-	case 3:
-		hd44780_cmd(0xD4);
-		break;
-	default:
-		break;
-	}
+    switch(line) {
+        case 0:
+            hd44780_cmd(0x80);
+            break;
+        case 1:
+            hd44780_cmd(0xC0);
+            break;
+        case 2:
+            hd44780_cmd(0x94);
+            break;
+        case 3:
+            hd44780_cmd(0xD4);
+            break;
+        default:
+            break;
+    }
 }
 
 void hd44780_go_to(u8 row, u8 col) {
@@ -86,63 +86,63 @@ void hd44780_go_to(u8 row, u8 col) {
 }
 
 void hd44780_cgram_write(u8 pos, u8 data_[8]) {
-	if (pos > 7) {
-		return;
-	}
-	pos = 64 + pos * 8;
-	hd44780_cmd(pos);
-	u8 i;
-	for (i = 0; i < 8; ++i) {
-		hd44780_send(data_[i], true);
-	}
+    if (pos > 7) {
+        return;
+    }
+    pos = 64 + pos * 8;
+    hd44780_cmd(pos);
+    u8 i;
+    for (i = 0; i < 8; ++i) {
+    	hd44780_send(data_[i], true);
+    }
 }
 
 void hd44780_init(TIM_TypeDef *t) {
-	timer = t;
-	// Setup clock
-        setup_delay_timer(timer);
+    timer = t;
+    // Setup clock
+    setup_delay_timer(timer);
 
-	hd44780_data[0] = 0x00 | backlight;
-	hd44780_data[1] = 0x00 | backlight;
+    hd44780_data[0] = 0x00 | backlight;
+    hd44780_data[1] = 0x00 | backlight;
 
-        // Init I2C
-        I2C_LowLevel_Init(I2C1);
+    // Init I2C
+    I2C_LowLevel_Init(I2C1);
 
-	// Reset all
-	I2C_Master_BufferWrite(I2C1, hd44780_data, 1, Polling, hd44780_address << 1);
+    // Reset all
+    I2C_Master_BufferWrite(I2C1, hd44780_data, 1, Polling, hd44780_address << 1);
 
-	hd44780_cmd(0x03);
-	delay_us(timer, 5000);
-	hd44780_cmd(0x03);
-	delay_us(timer, 100);
-	hd44780_cmd(0x02);
-	delay_us(timer, 200);
+    hd44780_cmd(0x03);
+    delay_us(timer, 5000);
+    hd44780_cmd(0x03);
+    delay_us(timer, 100);
+    hd44780_cmd(0x02);
+    delay_us(timer, 200);
 
-	hd44780_cmd(0x28); // 4 bit mode
-	hd44780_cmd(0x06); // set direction of cursor to right
-	hd44780_cmd(0x01); // clear display, go to 0x0
-	hd44780_cmd(0x0E); // turn on display, set solid cursor
-	hd44780_cmd(0x0C); // turn on display, set invisiblecursor
+    hd44780_cmd(0x28); // 4 bit mode
+    hd44780_cmd(0x06); // set direction of cursor to right
+    hd44780_cmd(0x01); // clear display, go to 0x0
+    hd44780_cmd(0x0E); // turn on display, set solid cursor
+    hd44780_cmd(0x0C); // turn on display, set invisiblecursor
 
-        u8 arr[] = {0,10,31,31,14,4,0,0};
-        u8 arr2[]= {0,10,21,17,10,4,0,0};
+//    u8 arr[] = {0,10,31,31,14,4,0,0};
+//    u8 arr2[]= {0,10,21,17,10,4,0,0};
 
-	hd44780_cgram_write(0, arr);
-	hd44780_cgram_write(1, arr2);
+//    hd44780_cgram_write(0, arr);
+//    hd44780_cgram_write(1, arr2);
 
-	hd44780_cmd(0x01); // clear display, go to 0x0
+    hd44780_cmd(0x01); // clear display, go to 0x0
 
-	hd44780_backlight(true);
-//	hd44780_print("Linia 0");
-//	hd44780_go_to_line(1);
-//	hd44780_print("Linia 1");
-//	hd44780_go_to_line(2);
-//	hd44780_print("Linia 2");
-//	hd44780_go_to_line(3);
-//	hd44780_print("Linia 3");
+    hd44780_backlight(true);
+//    hd44780_print("Linia 0");
+//    hd44780_go_to_line(1);
+//    hd44780_print("Linia 1");
+//    hd44780_go_to_line(2);
+//    hd44780_print("Linia 2");
+//    hd44780_go_to_line(3);
+//    hd44780_print("Linia 3");
 
-//	hd44780_char(0);
-//	hd44780_char(1);
+//    hd44780_char(0);
+//    hd44780_char(1);
 }
 
 
--- hd44780-i2c.h
+++ hd44780-i2c.h
@@ -10,6 +10,7 @@
 
 #define hd44780_address 0x27
 void hd44780_init(TIM_TypeDef *timer);
+void hd44780_cgram_write(u8 pos, u8 data_[8]);
 void hd44780_print(char *string);
 void hd44780_go_to_line(u8 line);
 void hd44780_go_to(u8 row, u8 col);
