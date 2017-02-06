869c8822dd866bed8e44fb145ad7efb95f49811e
Karol Werner <karol@ppkt.eu>
2017-02-06 18:32:59 +0100

    MAX7219: Refactor library to support SPI2 as well

--- max7219.c
+++ max7219.c
@@ -3,89 +3,101 @@
 static uint8_t data[8];
 
 // Set pin as soft NSS
-void max7219_gpio_configuration(void) {
+void max7219_gpio_configuration(max7219_device *device) {
     // Configure SPI1 NSS pin
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    if (device->spi == SPI1) {
+        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+        GPIO_Init(GPIOA, &GPIO_InitStructure);
+    } else if (device->spi == SPI2) {
+        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+        GPIO_Init(GPIOB, &GPIO_InitStructure);
+    }
 }
 
 // Send one byte of data to selected address
-void max7219_send(uint8_t address, uint8_t value) {
+void max7219_send(max7219_device* device, uint8_t address, uint8_t value) {
     static uint8_t tx[2];
     static uint8_t rx[2];
 
     tx[0] = address;
     tx[1] = value;
-    GPIO_WriteBit(GPIOA, GPIO_Pin_4, Bit_RESET);
-    spi_send(SPI1, tx, rx, 2);
-    GPIO_WriteBit(GPIOA, GPIO_Pin_4, Bit_SET);
+
+    spi_nss(device->spi, false);
+    spi_send(device->spi, tx, rx, 2);
+    spi_nss(device->spi, true);
 }
 
 // Initialize module and peripherals
-void max7219_init(void) {
-    spi_init(SPI1);
-    max7219_gpio_configuration();
+max7219_device *max7219_init(SPI_TypeDef *spi) {
+    max7219_device* device = malloc(sizeof(max7219_device));
+    device->spi = spi;
 
-    max7219_reset();
+    spi_init(device->spi);
+    max7219_gpio_configuration(device);
+
+    max7219_reset(device);
 
     // Enable all channels
-    max7219_send(REG_SCAN_LIMIT, 7);
+    max7219_send(device, REG_SCAN_LIMIT, 7);
 
-    max7219_clear_display();
+    max7219_clear_display(device);
+
+    return device;
 }
 
 // Perform reset operation
-void max7219_reset(void) {
+void max7219_reset(max7219_device* device) {
     // Shutdown
-    max7219_send(REG_SHUTDOWN, 0x00);
+    max7219_send(device, REG_SHUTDOWN, 0x00);
 
     // Normal operation
-    max7219_send(REG_SHUTDOWN, 0x01);
+    max7219_send(device, REG_SHUTDOWN, 0x01);
 }
 
 // Turn on briefly all LEDs on board
-void max7219_self_test(void) {
+void max7219_self_test(max7219_device* device) {
     // Turn on all LEDs
-    max7219_send(REG_TEST, 0x01);
+    max7219_send(device, REG_TEST, 0x01);
 
     uint32_t i;
     for (i = 0; i < 0x200000; ++i);
 
     // Return to normal operation
-    max7219_send(REG_TEST, 0x00);
+    max7219_send(device, REG_TEST, 0x00);
 }
 
 // Turn off all LEDs
-void max7219_clear_display(void) {
+void max7219_clear_display(max7219_device* device) {
     static uint8_t row;
     for (row = 0; row < 8; ++row) {
         data[row] = 0x00;
-        max7219_send(REG_CHANNEL0 + row, data[row]);
+        max7219_send(device, REG_CHANNEL0 + row, data[row]);
     }
 }
 
 // Turn on all LEDs
-void max7219_turn_all(void) {
+void max7219_turn_all(max7219_device* device) {
     static uint8_t row;
     for (row = 0; row < 8; ++row) {
         data[row] = 0xFF;
-        max7219_send(REG_CHANNEL0 + row, data[row]);
+        max7219_send(device, REG_CHANNEL0 + row, data[row]);
     }
 }
 
 // Set new intensity of LEDs
-void max7219_set_brightness(uint8_t new_value) {
+void max7219_set_brightness(max7219_device* device, uint8_t new_value) {
     if (new_value > 0x0F) {
         new_value = 0x0F;
     }
-    max7219_send(REG_INTENSITY, new_value);
+    max7219_send(device, REG_INTENSITY, new_value);
 }
 
 // Set new value for single LED
-static void max7219_new_value(uint8_t row, uint8_t column, bool value) {
+static void max7219_new_value(max7219_device* device, uint8_t row, uint8_t column, bool value) {
     if (row >= 8 || column >= 8) return;
 
     // columns ale numbered from right, so we have invert them
@@ -96,24 +108,24 @@ static void max7219_new_value(uint8_t row, uint8_t column, bool value) {
     else
         data[row] &= ~(1 << column);
 
-    max7219_send(REG_CHANNEL0 + row, data[row]);
+    max7219_send(device, REG_CHANNEL0 + row, data[row]);
 }
 
 // Turn on single LED
-void max7219_set_led(uint8_t row, uint8_t column) {
-    max7219_new_value(row, column, true);
+void max7219_set_led(max7219_device* device, uint8_t row, uint8_t column) {
+    max7219_new_value(device, row, column, true);
 }
 
 // Turn off single LED
-void max7219_reset_led(uint8_t row, uint8_t column) {
-    max7219_new_value(row, column, false);
+void max7219_reset_led(max7219_device* device, uint8_t row, uint8_t column) {
+    max7219_new_value(device, row, column, false);
 }
 
 // Draw whole 8x8 segment
-void max7219_set_data(uint8_t new_data[8]) {
+void max7219_set_data(max7219_device* device, uint8_t new_data[8]) {
     static uint8_t row;
     for (row = 0; row < 8; ++row) {
         data[row] = new_data[row];
-        max7219_send(REG_CHANNEL0 + row, data[row]);
+        max7219_send(device, REG_CHANNEL0 + row, data[row]);
     }
 }
--- max7219.h
+++ max7219.h
@@ -1,22 +1,31 @@
 #ifndef __MAX7219_H__
 #define __MAX7219_H__
 
+#include <stdlib.h>
+
 #include "common_lib/spi.h"
 
-#define REG_CHANNEL0        0x01
-#define REG_INTENSITY       0x0A
-#define REG_SCAN_LIMIT      0x0B
-#define REG_SHUTDOWN        0x0C
-#define REG_TEST            0x0F
 
-void max7219_init(void);
-void max7219_reset(void);
-void max7219_self_test(void);
-void max7219_clear_display(void);
-void max7219_set_brightness(uint8_t new_value);
-void max7219_turn_all(void);
-void max7219_set_led(uint8_t row, uint8_t column);
-void max7219_reset_led(uint8_t row, uint8_t column);
-void max7219_set_data(uint8_t new_data[8]);
+typedef enum {
+    REG_CHANNEL0 = 0x01,
+    REG_INTENSITY = 0x0A,
+    REG_SCAN_LIMIT = 0x0B,
+    REG_SHUTDOWN = 0x0C,
+    REG_TEST = 0x0F,
+} MAX7219_REGISTER;
+
+typedef struct {
+    SPI_TypeDef *spi;
+} max7219_device;
+
+max7219_device* max7219_init(SPI_TypeDef *spi);
+void max7219_reset(max7219_device* device);
+void max7219_self_test(max7219_device *device);
+void max7219_clear_display(max7219_device* device);
+void max7219_set_brightness(max7219_device* device, uint8_t new_value);
+void max7219_turn_all(max7219_device* device);
+void max7219_set_led(max7219_device* device, uint8_t row, uint8_t column);
+void max7219_reset_led(max7219_device* device, uint8_t row, uint8_t column);
+void max7219_set_data(max7219_device* device, uint8_t new_data[8]);
 
 #endif // __MAX7219_H__
