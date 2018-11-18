95a2cfd46ef5158c3c7b35e7fd61e77a20ea69be
Karol Werner <karol@ppkt.eu>
2018-11-18 16:45:14 +0100

    MPU9255: Add support for AK8963 magnetometer

--- mpu9255.c
+++ mpu9255.c
@@ -4,22 +4,25 @@ static uint8_t tx[2] = {0,};
 static uint8_t rx[6] = {0,};
 
 // Send single byte to device
-void i2c_sendbyte(mpu9255_device *device, uint8_t reg, uint8_t data) {
+void i2c_sendbyte(const i2c_device *device, uint8_t reg, uint8_t data) {
     tx[0] = reg;
     tx[1] = data;
     I2C_Master_BufferWrite(device->i2c, tx, 2, Polling, device->address << 1);
 }
 
 // Read single byte from device
-uint8_t i2c_readbyte(mpu9255_device *device, uint8_t reg) {
+uint8_t i2c_readbyte(const i2c_device *device, uint8_t reg) {
     tx[0] = reg;
-    i2c_master_transaction_write_read(device->i2c, device->address,
-            tx, 1, rx, 1, Polling);
+    Status s = i2c_master_transaction_write_read(device->i2c, device->address,
+                                                 tx, 1, rx, 1, Polling);
+    if (s == Error)
+        hacf();
+
     return rx[0];
 }
 
 // Read n bytes from device
-uint8_t *i2c_readbytes(mpu9255_device *device, uint8_t reg, uint8_t bytes) {
+uint8_t *i2c_readbytes(const i2c_device *device, uint8_t reg, uint8_t bytes) {
     tx[0] = reg;
     uint8_t *rx_data = calloc(bytes, sizeof(uint8_t));
     i2c_master_transaction_write_read(device->i2c, device->address,
@@ -28,10 +31,10 @@ uint8_t *i2c_readbytes(mpu9255_device *device, uint8_t reg, uint8_t bytes) {
 }
 
 // Perform initialization
-mpu9255_device *mpu9255_init(I2C_TypeDef *I2Cx, TIM_TypeDef *timer) {
+i2c_device *mpu9255_init(I2C_TypeDef *I2Cx, TIM_TypeDef *timer) {
     I2C_LowLevel_Init(I2Cx);
 
-    mpu9255_device *dev = malloc(sizeof(mpu9255_device));
+    i2c_device *dev = malloc(sizeof(i2c_device));
     dev->i2c = I2Cx;
     dev->address = MPU9255_ADDRESS;
     dev->timer = timer;
@@ -39,13 +42,13 @@ mpu9255_device *mpu9255_init(I2C_TypeDef *I2Cx, TIM_TypeDef *timer) {
 }
 
 // Check if device is present on I2C bus
-bool mpu9255_check_presence(mpu9255_device *device) {
+bool mpu9255_check_presence(const i2c_device *device) {
     return i2c_readbyte(device, MPU9255_WHO_AM_I) == 0x73;
 }
 
 // Perform self test of device based on implementation from:
 // https://github.com/kriswiner/MPU-9250
-void mpu9255_self_test(mpu9255_device *device) {
+void mpu9255_self_test(const i2c_device *device) {
     // set sample divider to 1 kHz
     i2c_sendbyte(device, MPU9255_SMPLRT_DIV, 0x00);
 
@@ -156,34 +159,33 @@ void mpu9255_self_test(mpu9255_device *device) {
     }
 }
 
-void mpu9255_configure(mpu9255_device *device) {
+void mpu9255_configure(const i2c_device *device) {
     i2c_sendbyte(device, MPU9255_PWR_MGMT_1, 0x00);
     delay_ms(device->timer, 100);
 
     i2c_sendbyte(device, MPU9255_PWR_MGMT_1, 0x01);
     delay_ms(device->timer, 200);
 
-    i2c_sendbyte(device, MPU9255_CONFIG, 0x03);
+    mpu9255_config_reg.dlpf_cfg = 0b11;
+    i2c_sendbyte(device, MPU9255_CONFIG, mpu9255_config_reg.raw);
 
     i2c_sendbyte(device, MPU9255_SMPLRT_DIV, 0x04);
 
-    uint8_t gyro_config = i2c_readbyte(device, MPU9255_GYRO_CONFIG);
-    // Clear Fchoice bits [1:0]
-    gyro_config = gyro_config & (uint8_t) ~0x02;
-    // Clear AFS bits [4:3]
-    gyro_config = gyro_config & (uint8_t) ~0x18;
-    i2c_sendbyte(device, MPU9255_GYRO_CONFIG, gyro_config);
+    mpu9255_gyro_config_reg.raw = i2c_readbyte(device, MPU9255_GYRO_CONFIG);
+    // Clear Fchoice bits
+    mpu9255_gyro_config_reg.fchoice_b = 0b00;
+    // Clear AFS bits
+    mpu9255_gyro_config_reg.gyro_fs_sel = 0b00;
+    i2c_sendbyte(device, MPU9255_GYRO_CONFIG, mpu9255_gyro_config_reg.raw);
 
-    uint8_t accel_config = i2c_readbyte(device, MPU9255_ACCEL_CONFIG);
-    accel_config = accel_config & (uint8_t) ~0x18;
-    i2c_sendbyte(device, MPU9255_ACCEL_CONFIG, accel_config);
+    mpu9255_accelerometer_config_reg.raw = i2c_readbyte(device, MPU9255_ACCEL_CONFIG);
+    mpu9255_accelerometer_config_reg.accel_fs_sel = 0b00;
+    i2c_sendbyte(device, MPU9255_ACCEL_CONFIG, mpu9255_accelerometer_config_reg.raw);
 
-    accel_config = i2c_readbyte(device, MPU9255_ACCEL_CONFIG2);
-    // Clear accel_fchoice_b (bit 3) and A_DLPFG (bits [2:0])
-    accel_config = accel_config & (uint8_t) ~0x0F;
-    // Set accelerometer rate to 1 kHz and bandwidth to 41 Hz
-    accel_config = accel_config | (uint8_t) 0x03;
-    i2c_sendbyte(device, MPU9255_ACCEL_CONFIG2, accel_config);
+    mpu9255_accelerometer_config2_reg.raw = i2c_readbyte(device, MPU9255_ACCEL_CONFIG2);
+    mpu9255_accelerometer_config2_reg.accel_fchoice_b = 0b0;
+    mpu9255_accelerometer_config2_reg.A_DLPFCFG = 0b000;
+    i2c_sendbyte(device, MPU9255_ACCEL_CONFIG2, mpu9255_accelerometer_config2_reg.raw);
 
     i2c_sendbyte(device, MPU9255_INT_PIN_CFG, 0x22);
     i2c_sendbyte(device, MPU9255_INT_ENABLE, 0x01);
@@ -191,7 +193,7 @@ void mpu9255_configure(mpu9255_device *device) {
     delay_ms(device->timer, 200);
 }
 
-int16_t *mpu9255_read_accelerometer_data(mpu9255_device *device) {
+int16_t *mpu9255_read_accelerometer_data(const i2c_device *device) {
     uint8_t *raw_accelerometer_data = i2c_readbytes(device, MPU9255_ACCEL_XOUT_H, 6);
     int16_t *accelerometer_data = calloc(3, sizeof(uint16_t));
 
@@ -204,7 +206,7 @@ int16_t *mpu9255_read_accelerometer_data(mpu9255_device *device) {
     return accelerometer_data;
 }
 
-int16_t *mpu9255_read_gyroscope_data(mpu9255_device *device) {
+int16_t *mpu9255_read_gyroscope_data(const i2c_device *device) {
     uint8_t *raw_gyroscope_data = i2c_readbytes(device, MPU9255_GYRO_XOUT_H, 6);
     int16_t *gyroscope_data = calloc(3, sizeof(uint16_t));
     for (uint8_t i = 0; i < 3; ++i) {
@@ -213,4 +215,118 @@ int16_t *mpu9255_read_gyroscope_data(mpu9255_device *device) {
     }
     free(raw_gyroscope_data);
     return gyroscope_data;
+}
+
+float mpu9255_read_temperature(const i2c_device *device) {
+    static uint16_t raw_temp;
+
+    uint8_t *raw_temp_data = i2c_readbytes(device, MPU9255_TEMP_OUT_H, 2);
+    raw_temp = raw_temp_data[0] << 8 | raw_temp_data[1];
+    free(raw_temp_data);
+    return raw_temp / 333.87f + 21.0f;
+}
+
+i2c_device *ak8963_init(I2C_TypeDef *I2Cx, TIM_TypeDef *timer) {
+    i2c_device *dev = malloc(sizeof(i2c_device));
+    dev->i2c = I2Cx;
+    dev->address = AK8963_ADDRESS;
+    dev->timer = timer;
+    return dev;
+}
+
+bool ak8963_check_presence(const i2c_device *device) {
+    return i2c_readbyte(device, AK8963_WIA) == 0x48;
+}
+
+int16_t *ak8963_read_magnetometer_raw(const i2c_device *device) {
+    uint8_t *data = i2c_readbytes(device, AK8963_HXL, 7);
+    int16_t *raw_data = calloc(3, sizeof(float));
+    raw_data[0] = data[1] << 8 | data[0];
+    raw_data[1] = data[3] << 8 | data[2];
+    raw_data[2] = data[5] << 8 | data[4];
+    free(data);
+    return raw_data;
+}
+
+bool ak8963_self_test(const i2c_device *device, const ak8963_settings *settings) {
+    // 1) Power down device
+    ak8963_control1_reg.raw = 0x00;
+    ak8963_control1_reg.bit = (uint8_t) settings->precision;
+    i2c_sendbyte(device, AK8963_CNTL1, ak8963_control1_reg.raw);
+
+    // 2) Write 1 to SELF in ASTC register
+    ak8963_self_test_reg.self = 1;
+    i2c_sendbyte(device, AK8963_ASTC, ak8963_self_test_reg.raw);
+
+    // 3) Set self-test mode
+    ak8963_control1_reg.mode = 0b1000;
+    i2c_sendbyte(device, AK8963_CNTL1, ak8963_control1_reg.raw);
+
+    // 4) Check for Data Ready
+    do {
+        ak8963_status1_reg.raw = i2c_readbyte(device, AK8963_ST1);
+    } while (ak8963_status1_reg.drdy != 1);
+
+    // 5) Read measurements
+    int16_t *data = ak8963_read_magnetometer_raw(device);
+    int16_t x = data[0], y = data[1], z = data[2];
+    free(data);
+
+    // 6) Write 0 to SELF bit in ASTC
+    ak8963_self_test_reg.self = 0;
+    i2c_sendbyte(device, AK8963_ASTC, ak8963_self_test_reg.raw);
+
+    // 7) Set Power-Down mode
+    ak8963_control1_reg.mode = 0b0000;
+    i2c_sendbyte(device, AK8963_CNTL1, ak8963_control1_reg.raw);
+
+    // Check if measurements are correct
+    if (settings->precision) {
+        // 16-bit values
+        if ((x <= 200 && x >= -200) &&
+            (y <= 200 && y >= -200) &&
+            (z <= -800 && x >= -3200)) {
+            return true;
+        }
+    } else {
+        // 14-bit values
+        if ((x <= 50 && x >= -50) &&
+            (y <= 50 && y >= -50) &&
+            (z <= -200 && x >= -800)) {
+            return true;
+        }
+    }
+    hacf();
+    return false;
+}
+
+float *ak8963_read_magnetometer_data(const i2c_device *device,
+                                     const ak8963_settings *settings) {
+    int16_t *data = ak8963_read_magnetometer_raw(device);
+    float lsb = settings->precision ? 0.15f : 0.6f;
+
+    float *readings = calloc(3, sizeof(float));
+    readings[0] = data[0] * lsb;
+    readings[1] = data[1] * lsb;
+    readings[2] = data[2] * lsb;
+    free(data);
+    return readings;
+}
+
+void ak8963_configure(const i2c_device *device, const ak8963_settings *settings) {
+    // Restart device
+    ak8963_control2_reg.raw = 0x00;
+    ak8963_control2_reg.srst = 1;
+    i2c_sendbyte(device, AK8963_CNTL2, ak8963_control2_reg.raw);
+    do {
+        print_variable_hex(ak8963_control2_reg.raw);
+        ak8963_control2_reg.raw = i2c_readbyte(device, AK8963_CNTL2);
+    } while (ak8963_control2_reg.srst != 0);
+
+    // Set mode to continuous measurement with provided precision
+    ak8963_control1_reg.raw = 0x00;
+    ak8963_control1_reg.bit = (uint8_t) settings->precision;
+    ak8963_control1_reg.mode = 0b0110;
+    print_variable_hex(ak8963_control1_reg.raw);
+    i2c_sendbyte(device, AK8963_CNTL1, ak8963_control1_reg.raw);
 }
--- mpu9255.h
+++ mpu9255.h
@@ -10,7 +10,8 @@
 #include "common_lib/usart.h"
 #include "common_lib/utils.h"
 
-#define MPU9255_ADDRESS 0x68
+#define MPU9255_ADDRESS 0x68  // accel+gyro+temp
+#define AK8963_ADDRESS 0x0C  // magnetometer
 
 enum mpu9255_register_map {
     MPU9255_SELF_TEST_X_GYRO = 0x00,
@@ -32,6 +33,7 @@ enum mpu9255_register_map {
 
     MPU9255_ACCEL_XOUT_H = 0x3B,
 
+    MPU9255_TEMP_OUT_H = 0x41,
     MPU9255_GYRO_XOUT_H = 0x43,
 
     MPU9255_PWR_MGMT_1 = 0x6B,
@@ -39,22 +41,141 @@ enum mpu9255_register_map {
     MPU9255_WHO_AM_I = 0x75,
 };
 
+enum ak8963_register_map {
+    AK8963_WIA = 0x00,
+    AK8963_INFO = 0x01,
+    AK8963_ST1 = 0x02,
+    AK8963_HXL = 0x03,
+    AK8963_ST2 = 0x09,
+    AK8963_CNTL1 = 0x0A,
+    AK8963_CNTL2 = 0x0B,
+    AK8963_ASTC = 0x0C,
+    AK8963_ASAX = 0x10,
+};
+
+union {
+    uint8_t raw;
+    struct __attribute__((packed)) {
+        uint8_t dlpf_cfg:3;
+        uint8_t ext_sync_set:3;
+        uint8_t fifo_mode:1;
+        uint8_t _:1;
+    };
+} mpu9255_config_reg;
+
+union {
+    uint8_t raw;
+    struct __attribute__((packed)) {
+        uint8_t fchoice_b:2;
+        uint8_t _:1;
+        uint8_t gyro_fs_sel:2;
+        uint8_t z_gyro_cten:1;
+        uint8_t y_gyro_cten:1;
+        uint8_t x_gyro_cten:1;
+    };
+} mpu9255_gyro_config_reg;
+
+union {
+    uint8_t raw;
+    struct __attribute__((packed)) {
+        uint8_t _:3;
+        uint8_t accel_fs_sel:2;
+        uint8_t az_st_en:1;
+        uint8_t ay_st_en:1;
+        uint8_t ax_st_en:1;
+    };
+} mpu9255_accelerometer_config_reg;
+
+
+union {
+    uint8_t raw;
+    struct __attribute__((packed)) {
+        uint8_t A_DLPFCFG:3;
+        uint8_t accel_fchoice_b:1;
+        uint8_t __:2;
+        uint8_t _:2;
+    };
+} mpu9255_accelerometer_config2_reg;
+
+union {
+    uint8_t raw;
+    struct __attribute__((packed)) {
+        uint8_t drdy:1;  // Data ready
+        uint8_t dor:1;  // Data overrun
+        uint8_t _:6;  // Read only!
+    };
+} ak8963_status1_reg;
+
+union {
+    uint8_t raw;
+    struct __attribute__((packed)) {
+        uint8_t _:3;
+        uint8_t hofl:1;  // Magnetic sensor overflow
+        uint8_t bitm:1;  // Mirror of ak8963_control1_reg.bit
+        uint8_t __:3;
+    };
+} ak8963_status2_reg;  // Read only!
+
+union {
+    uint8_t raw;
+    struct __attribute__((packed)) {
+        uint8_t mode:4;  // operation mode setting
+        uint8_t bit:1;  // Output bit setting - 0-14b, 1-16b
+        uint8_t _:3;
+    };
+} ak8963_control1_reg;
+
+union {
+    uint8_t raw;
+    struct __attribute__((packed)) {
+        uint8_t srst:1;  // soft reset
+        uint8_t _:7;
+    };
+} ak8963_control2_reg;
+
+union {
+    uint8_t raw;
+    struct __attribute__((packed)) {
+        uint8_t __:6;
+        uint8_t self:1;  // 1 - generate magnetic field for self-test
+        uint8_t _:1;
+    };
+} ak8963_self_test_reg;
+
+typedef struct {
+    bool precision;
+} ak8963_settings;
+
 typedef struct {
     uint8_t address;
     I2C_TypeDef *i2c;
     TIM_TypeDef *timer;
-} mpu9255_device;
+} i2c_device;
 
-mpu9255_device *mpu9255_init(I2C_TypeDef *I2Cx, TIM_TypeDef *timer);
+i2c_device *mpu9255_init(I2C_TypeDef *I2Cx, TIM_TypeDef *timer);
 
-bool mpu9255_check_presence(mpu9255_device *device);
+bool mpu9255_check_presence(const i2c_device *device);
 
-void mpu9255_self_test(mpu9255_device *device);
+void mpu9255_self_test(const i2c_device *device);
 
-void mpu9255_configure(mpu9255_device *device);
+void mpu9255_configure(const i2c_device *device);
 
-int16_t *mpu9255_read_accelerometer_data(mpu9255_device *device);
+int16_t *mpu9255_read_accelerometer_data(const i2c_device *device);
 
-int16_t *mpu9255_read_gyroscope_data(mpu9255_device *device);
+int16_t *mpu9255_read_gyroscope_data(const i2c_device *device);
+
+float mpu9255_read_temperature(const i2c_device *device);
+
+i2c_device *ak8963_init(I2C_TypeDef *I2Cx, TIM_TypeDef *timer);
+
+void ak8963_configure(const i2c_device *device,
+                      const ak8963_settings *settings);
+
+bool ak8963_check_presence(const i2c_device *device);
+
+bool ak8963_self_test(const i2c_device *device, const ak8963_settings *settings);
+
+float *ak8963_read_magnetometer_data(const i2c_device *device,
+                                     const ak8963_settings *settings);
 
 #endif // __MPU9250_H__
